96d39d500e90935442752cdd4d9796c11753e038
CGMcBride <connermcbride@ymail.com>
2020-06-23 10:21:06 -0500

    add my js file

---
+++ index.js
@@ -0,0 +1,13 @@
+function myFunction(name) {
+    console.log("hello my name is " + name)
+};
+
+myFunction("conner")
+
+const waterPlant = function (days) {
+    if (days === "tuesday") {
+        return true;
+    } else {
+        return false
+    }
+}
